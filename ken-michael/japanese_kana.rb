5edecb01b8334e63e5068110c58f9ff8310fead1
Michael Reinsch <michael@mobalean.com>
2012-10-21 15:22:35 +0900

    final

--- ken-michael/japanese_kana.rb
+++ ken-michael/japanese_kana.rb
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-module JapaneseNumericSub
+
+require "nkf"
+
+module JapaneseKana
   RAW_TABLE = {
     %w[まる ま れい れ オウ ゼロ ゼ] => 0,
     %w[ひとつ ひと ひ いち い ワン] => 1,
@@ -10,33 +13,35 @@ module JapaneseNumericSub
     %w[むつ む ろく ろ シックス] => 6,
     %w[ななつ なな な しち セブン セヴン] => 7,
     %w[やつ や はち は ば エート] => 8,
-    %w[ここのつ こ きゅう く ナイン] => 9,
+    %w[ここのつ こ きゅう キュー く ナイン] => 9,
     %w[とお じゅう じ テン] => 10 }
 
   TABLE = {}
-  RAW_TABLE.each{|keys,v| keys.each{|k| TABLE[k] = v.to_s }}
+  RAW_TABLE.each{|keys,v| keys.each{|k| TABLE[NKF.nkf("-w -h2", k)] = v.to_s }}
 
-  def self.sub(word)
+  def self.to_num(word)
     4.downto(1).each do |count|
       TABLE.find_all {|k,v| k.length == count }.each do |k,v|
-        puts "checking for #{k},#{v}"
         word = word.gsub(k,v)
       end
     end
     word
   end
-
 end
 
 
 require "rspec"
 
+describe JapaneseKana do
 
-describe JapaneseNumericSub do
-
-  it { JapaneseNumericSub.sub("ゼロツ").should == "02" }
-  
-  [ %w[オハヨウ 0840], %w[アイシテイル 14106], %w[ナニシテイル 724106], %w[サヨナラ 3470] ].each do |w,n|
-    it { JapaneseNumericSub.sub(w).should == n }
+  [ %w[ゼロツ 02],
+    %w[ヨロシク 4649],
+    %w[イヤナヤツ 1878],
+    %w[ハヤク 889],
+    %w[ナゴヤ 758],
+    %w[クサイ 931],
+    %w[サイゴ 315],
+    %w[サンキュー 39] ].each do |w,n|
+    it("should convert #{w} to #{n}") { JapaneseKana.to_num(w).should == n }
   end
 end
