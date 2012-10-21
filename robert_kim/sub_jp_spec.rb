c0f23ebc4ed1d0d5701b76cf6692d0fdc60f787e
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2012-10-21 15:09:15 +0900

    Supports simple katakana to number words.

--- robert_kim/sub_jp_spec.rb
+++ robert_kim/sub_jp_spec.rb
@@ -6,11 +6,11 @@ require "#{File.dirname(__FILE__)}/sub_jp"
 
 describe SubstituteJp do
   it "should return nil" do
-    SubstituteJp.numericalize("hoge").should be_nil
+    SubstituteJp.numericalize("hoge").should eq ""
   end
   context "when given 'ヨロシク'" do
     it "should return 4649" do
-    SubstituteJp.numericalize("ヨロシク").should eq 4649
+    SubstituteJp.numericalize("ヨロシク").should eq "4649"
     end
   end
 end
